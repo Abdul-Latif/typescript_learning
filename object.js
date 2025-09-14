6450ab8d9932dc0b87cb757746031087ed9cad4f
Abdullatif <m3yiel@gmail.com>
2025-09-14 16:52:58 +0300

    objects

--- object.js
+++ object.js
@@ -1,3 +1,47 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=object.js.map
+// objects annotations
+var object = {
+    username: "osama",
+    id: 101,
+    status: true
+};
+//updating data 
+object.username = "ahmed";
+object.id = 102;
+// object.id = "d"; // error
+object.status = false;
+console.log(object.username);
+console.log(object.id);
+console.log(object.status);
+// nested objects
+var object1 = {
+    username: "osama",
+    id: 101,
+    status: true,
+    skills: {
+        skill1: "html",
+        skill2: "css"
+    }
+};
+var object2 = {
+    username: "ahmed",
+    name: "hello",
+    age: 50,
+    status: true,
+    skills: {
+        skill1: "html",
+        skill2: "css",
+        skill3: "js"
+    }
+};
+object2.username = "osama";
+object2.name = "all";
+object2.age = 30;
+object2.skills.skill1 = "nothing";
+console.log(object2.username);
+console.log(object2.name);
+console.log(object2.age);
+console.log(object2.skills);
+console.log(object2.skills.skill1);
+// console.log(object1.skills.skill2);
+// console.log(object2.age);
+// console.log(object2.skills.skill3);
